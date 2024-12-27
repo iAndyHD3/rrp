459ec1ce20d51a2ec322580df8f942a13a6d6852
iAndyHD3 <54410739+iAndyHD3@users.noreply.github.com>
2024-12-27 01:35:21 +0100

    change impl

--- include/rrp/rrp.hpp
+++ include/rrp/rrp.hpp
@@ -1,57 +1,35 @@
 #pragma once
-
-#include "reflect"
-#include <exception>
+#include <source_location>
 #include <type_traits>
-#include "fast_float.h"
-#include <concepts>
-#include <iostream>
-
-#ifndef RRP_STRING_TYPE
-#include <string>
-#define RRP_STRING_TYPE std::string
-#endif
-
-
-#ifndef RRP_STRING_VIEW_TYPE
-#include <string_view>
-#define RRP_STRING_VIEW_TYPE std::string_view
-#endif
-
-
-#ifndef RRP_VECTOR_TYPE
+#include <unordered_map>
 #include <vector>
-#define RRP_VECTOR_TYPE std::vector
-#endif
-
-
-#ifndef RRP_OPTIONAL_TYPE
+#include <string>
+#include <string_view>
+#include "reflect"
+#include <iostream>
+#include "fast_float.h"
 #include <optional>
-#define RRP_OPTIONAL_TYPE std::optional
-#endif
-
 
 namespace rrp
 {
 
-template<typename T>
-using vector = RRP_VECTOR_TYPE<T>;
-
-template<typename T>
-using optional = RRP_OPTIONAL_TYPE<T>;
-
-using string = RRP_STRING_TYPE;
-using string_view = RRP_STRING_VIEW_TYPE;
-
-
-template<typename T>
-rrp::optional<T> fromString(rrp::string_view str)
+inline void myprint(auto a, std::source_location x = std::source_location::current())
 {
-	if constexpr (std::is_same_v<T, bool>)
+    std::cout << a << " | " << x.file_name() << ":" << x.line() << '\n';
+}
+
+template<typename T>
+std::optional<T> fromString(std::string_view str)
+{
+    if constexpr (std::is_same_v<T, std::string>)
+    {
+        return std::string(str);
+    }
+    else if constexpr (std::is_same_v<T, bool>)
 	{
 		return str == "1" || str == "true";
 	}
-	else
+    else
 	{
 		T value;
 		if (fast_float::from_chars(str.data(), str.data() + str.size(), value).ec == std::errc())
@@ -61,94 +39,281 @@ rrp::optional<T> fromString(rrp::string_view str)
 	}
 	return {};
 }
-
-
-struct Response
+inline std::vector<std::string_view> splitByDelimStringView(std::string_view str, std::string_view delim)
 {
-    int a;
-    int b;
-    int c;
-    static constexpr rrp::string_view DELIM = ",";
-};
-
-
-// Define a concept that matches numerical types but excludes char
-inline rrp::vector<rrp::string_view> splitByDelimStringView(std::string_view str, std::string_view delim)
-{
-
-	rrp::vector<rrp::string_view> tokens;
+	std::vector<std::string_view> tokens;
 	size_t pos = 0;
 	size_t len = str.length();
 
 	while (pos < len)
 	{
 		size_t end = str.find(delim, pos);
-		if(end == std::string_view::npos)
+		if (end == std::string_view::npos)
 		{
 			tokens.emplace_back(str.substr(pos));
 			break;
 		}
 		tokens.emplace_back(str.substr(pos, end - pos));
-		pos = end + delim.size();
+		pos = end + 1;
 	}
 
 	return tokens;
 }
+
+inline std::unordered_map<int, std::string_view> splitByDelimToMap(std::string_view str, std::string_view delim)
+{
+    std::unordered_map<int, std::string_view> tokens;
+    size_t pos = 0;
+    size_t len = str.length();
+
+    while (pos < len)
+    {
+        // Find the position of the delimiter for the key
+        size_t key_end = str.find(delim, pos);
+        if (key_end == std::string_view::npos) break; // No more delimiters found, end of string
+
+        // Extract the key
+        std::string_view key = str.substr(pos, key_end - pos);
+        pos = key_end + delim.length(); // Move past the delimiter
+
+        // Find the position of the delimiter for the value
+        size_t value_end = str.find(delim, pos);
+        if (value_end == std::string_view::npos) {
+            // If no delimiter found for value, it means value is until the end of string
+            std::string_view value = str.substr(pos);
+            if (auto k = fromString<int>(key)) tokens.emplace(*k, value);
+            break;
+        }
+
+        // Extract the value
+        std::string_view value = str.substr(pos, value_end - pos);
+        pos = value_end + delim.length(); // Move past the delimiter
+
+        // Insert into map if key can be converted to int
+        if (auto k = fromString<int>(key)) tokens.emplace(*k, value);
+    }
+
+    return tokens;
+}
+
+
+template<typename T, int index>
+struct IndexedValueMap
+{
+    using type = T;
+    T value{};
+    operator T() { return value; }
+    T* operator->() { return value; }
+    auto operator<=>(const T& rhs) const {
+        return value <=> rhs;
+    }
+
+    constexpr static int KEY = index;
+    static auto parse(std::string_view str)
+    {
+        IndexedValueMap<T, index> ret;
+        if(auto v = fromString<T>(str)) ret.value = *v;
+        return ret;
+    }
+};
+
+
+struct ValueMapContainer
+{
+    template<typename T>
+    static T parse(std::string_view str)
+    {
+        myprint("\n");
+        myprint(str);
+        T ret;
+        auto tokens = splitByDelimToMap(str, T::DELIM);
+        myprint(tokens.size());
+        for(const auto& k : tokens)
+        {
+            std::string x = std::to_string(k.first) + ":" + std::string(k.second);
+            myprint(x);
+        }
+        reflect::for_each([&](auto I){
+            using M = std::remove_reference_t<decltype(reflect::get<I>(ret))>;
+            auto&& member = reflect::get<I>(ret);
+            if(auto value_it = tokens.find(M::KEY); value_it != tokens.end())
+            {
+                if(auto v2_parsed = fromString<typename M::type>(value_it->second))
+                {
+                    member.value = *v2_parsed;
+                }
+            }
+        }, ret);
+        return ret;
+    }
+};
+
+#define RRP_IVM_W_GETTER(T, index, name) \
+    IndexedValueMap<T, index> m_##name; \
+    T& name() { return m_##name.value; }
+
+
 
 // Define a concept that matches numerical types but excludes char
 template <typename T>
 concept is_numerical_generic = std::is_arithmetic_v<T> && !std::is_same_v<T, char>;
 
 template <typename T>
-concept has_non_empty_delim = requires {
-    { T::DELIM } -> std::convertible_to<std::string_view>;
-} && (!T::DELIM.empty());
+concept has_non_empty_delim = requires {!T::DELIM.size() != 0;};
 
+
+
+#define RRP_DBC_W_GETTER(T, delim, name) \
+    DelimBasedContainer<T, delim> m_##name; \
+    std::vector<T>& name() { return m_##name.values ;}
+
+
+
+struct DelimBasedContainerBase {};
+template<typename T, reflect::fixed_string D>
+struct DelimBasedContainer : DelimBasedContainerBase
+{
+    static constexpr reflect::fixed_string DELIM = D;
+    std::vector<T> values;
+    operator std::vector<T>() { return values; }
+
+    //level1,level2,
+    //where level: 0:1:2:name:3:desc
+    static auto parse(std::string_view str)
+    {
+        
+        DelimBasedContainer<T, D> ret;
+        if constexpr(has_non_empty_delim<T>)
+        {
+            myprint(str);
+            auto tokens = splitByDelimStringView(str, DELIM);
+            for(const auto& t : tokens)
+            {
+                ret.values.push_back(T::template parse<T>(t));
+            }
+        }
+        return ret;
+    }
+};
 
 template<typename T>
-concept other = std::constructible_from<T, rrp::string_view>;
+bool is_indexed_value_map_only()
+{
+    bool ret = true;
+    reflect::for_each<T>([&](auto I){
+        if (reflect::type_name(reflect::get<I>(std::declval<T>())) != "IndexedValueMap")
+        {
+            ret = false;
+            return;
+        }
+    });
+
+    return ret;
+}
+
 
 
 template<typename T>
 T rrp(std::string_view str)
 {
-    static_assert(has_non_empty_delim<T>, "Type needs to have a non empty static constexpr std::string_view DELIM member");
-
-    if constexpr(T::DELIM.empty())
-    {
-        return {};
-    }
-
-    T r;
-
-    auto splitted = splitByDelimStringView(str, T::DELIM);
-
-    auto it = splitted.begin();
-
+    T ret;
+    auto tokens = splitByDelimStringView(str, T::DELIM);
+    auto it = tokens.begin();
     reflect::for_each([&](auto I)
     {
-        using MemberType = std::remove_reference_t<decltype(reflect::get<I>(r))>;
-        if constexpr(is_numerical_generic<MemberType>)
-        {
-            auto opt = fromString<MemberType>(*it);
-            if(opt) reflect::get<I>(r) = *opt;
-        }
-        else if constexpr(has_non_empty_delim<MemberType>)
-        {
-            reflect::get<I>(r) = rrp<MemberType>(*it);
-        }
-        else if constexpr(std::constructible_from<MemberType, rrp::string_view>)
-        {
-            reflect::get<I>(r) = MemberType(*it);
-        }
+        if(it == tokens.end()) return;
 
-        ++it;
-        
-    }, r);
+        using M = std::remove_reference_t<decltype(reflect::get<I>(ret))>;
+        auto&& member = reflect::get<I>(ret);
+        auto&& member_name = reflect::type_name(member);
+        if constexpr(std::is_base_of_v<DelimBasedContainerBase, M>)
+        {
+            std::cout << "HELLO";
+            member = M::parse(*it);
+            ++it;
+        }
+        else if constexpr(std::is_base_of_v<ValueMapContainer, M>)
+        {
+            member = M::template parse<M>(*it);
+            ++it;
+        }
+    }, ret);
 
-    return r;
+    return ret;
 }
 
 
+
+template<reflect::fixed_string D = "">
+struct LevelObject : public ValueMapContainer
+{
+    static constexpr reflect::fixed_string DELIM = D;
+
+    // Integer getters
+    RRP_IVM_W_GETTER(int, 1, levelID);
+    RRP_IVM_W_GETTER(int, 5, version);
+    RRP_IVM_W_GETTER(int, 6, playerID);
+    RRP_IVM_W_GETTER(int, 8, difficultyDenominator);
+    RRP_IVM_W_GETTER(int, 9, difficultyNumerator);
+    RRP_IVM_W_GETTER(int, 10, downloads);
+    RRP_IVM_W_GETTER(int, 11, setCompletes); // Note: Removed in update 2.1
+    RRP_IVM_W_GETTER(int, 12, officialSong);
+    RRP_IVM_W_GETTER(int, 13, gameVersion);
+    RRP_IVM_W_GETTER(int, 14, likes);
+    RRP_IVM_W_GETTER(int, 15, length);
+    RRP_IVM_W_GETTER(int, 16, dislikes);
+    RRP_IVM_W_GETTER(int, 18, stars);
+    RRP_IVM_W_GETTER(int, 19, featureScore);
+    RRP_IVM_W_GETTER(int, 30, copiedID);
+    RRP_IVM_W_GETTER(int, 35, customSongID);
+    RRP_IVM_W_GETTER(int, 37, coins);
+    RRP_IVM_W_GETTER(int, 39, starsRequested);
+    RRP_IVM_W_GETTER(int, 41, dailyNumber); // Note: Specific to daily/weekly levels
+    RRP_IVM_W_GETTER(int, 42, epic);
+    RRP_IVM_W_GETTER(int, 43, demonDifficulty);
+    RRP_IVM_W_GETTER(int, 45, objects);
+    RRP_IVM_W_GETTER(int, 46, editorTime);
+    RRP_IVM_W_GETTER(int, 47, editorTimeCopies);
+    RRP_IVM_W_GETTER(int, 54, unknown);
+    RRP_IVM_W_GETTER(int, 57, verificationTime);
+
+    // Bool getters
+    RRP_IVM_W_GETTER(bool, 17, demon);
+    RRP_IVM_W_GETTER(bool, 25, auto_);
+    RRP_IVM_W_GETTER(bool, 31, twoPlayer);
+    RRP_IVM_W_GETTER(bool, 38, verifiedCoins);
+    RRP_IVM_W_GETTER(bool, 40, lowDetailMode);
+    RRP_IVM_W_GETTER(bool, 44, isGauntlet);
+
+    // String getters
+    RRP_IVM_W_GETTER(std::string, 2, levelName);
+    RRP_IVM_W_GETTER(std::string, 3, description);
+    RRP_IVM_W_GETTER(std::string, 4, levelString);
+    RRP_IVM_W_GETTER(std::string, 26, recordString); // Note: Unused
+    RRP_IVM_W_GETTER(std::string, 28, uploadDate);
+    RRP_IVM_W_GETTER(std::string, 29, updateDate);
+    RRP_IVM_W_GETTER(std::string, 36, extraString);
+    RRP_IVM_W_GETTER(std::string, 48, settingsString); // Note: Unused after early 2.1
+
+    // Encrypted String getters
+    RRP_IVM_W_GETTER(std::string, 27, password);
+
+    // Comma-Separated List getters
+    RRP_IVM_W_GETTER(std::string, 52, songIDs);
+    RRP_IVM_W_GETTER(std::string, 53, sfxIDs);
+};
+
+template<reflect::fixed_string D>
+struct CreatorObject
+{
+    static reflect::fixed_string DELIM = D;
+    //TODO simple comma separated values 
+};
+
+struct GJLevels21
+{
+    static constexpr reflect::fixed_string DELIM = "#";
+    RRP_DBC_W_GETTER(LevelObject<":">, "|", levels);
+};
 
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,137 +1,36 @@
 #include <rrp/rrp.hpp>
-#include <catch2/catch_test_macros.hpp>
+#include "ut.hpp"
 
-#define DELIM(str) constexpr static std::string_view DELIM = str;
-struct SimpleResponse
+using namespace rrp;
+using namespace boost::ut;
+
+
+int main()
 {
-    int a = 5;
-    int b = 6;
-    int c = 7;
-    constexpr static std::string_view DELIM = ",";
-};
 
-TEST_CASE("SimpleIntegers")
-{
-    auto sr = rrp::rrp<SimpleResponse>("1,2,3");
-    REQUIRE(sr.a == 1);
-    REQUIRE(sr.b == 2);
-    REQUIRE(sr.c == 3);
-}
+    "essential_funcs"_test = [](){
+        auto tokens = splitByDelimToMap("0:a:1:bb:2:ccc:3:dddd", ":");
+        expect(tokens[0] == "a");
+        expect(tokens[1] == "bb");
+        expect(tokens[2] == "ccc");
+        expect(tokens[3] == "dddd");
+    };
 
-struct StringDelim
-{
-    int a, b, c;
-    constexpr static std::string_view DELIM = "---";
-};
+    "rrp"_test = [](){
+        auto r = rrp::rrp<GJLevels21>("1:10565740:2:Bloodbath:5:3:6:503085:8:10:9:50:10:44138442:12:0:13:21:14:2375318:17:1:43:6:25::18:10:19:10330:42:0:45:24746:3:V2hvc2UgYmxvb2Qgd2lsbCBiZSBzcGlsdCBpbiB0aGUgQmxvb2RiYXRoPyBXaG8gd2lsbCB0aGUgdmljdG9ycyBiZT8gSG93IG1hbnkgd2lsbCBzdXJ2aXZlPyBHb29kIGx1Y2suLi4=:15:3:30:7679228:31:0:37:0:38:0:39:0:46:1:47:2:35:467339|1:21761387:2:Bloodbath Z:5:1:6:3277407:8:10:9:20:10:4556435:12:0:13:20:14:200369:17:1:43:4:25::18:10:19:17840:42:0:45:0:3:UmVtYWtlIG9mIEJCLCBidXQgU2hvcnRlciBhbmQgbXVjaCBlYXNpZXIgWEQgTW9yZSBvZiBhIGdhbWVwbGF5IGxldmVsISAgSnVzdCBhIGZ1biBlYXN5IGRlbW9uLiBWZXJpZmllZCBCeSBYaW9kYXplciEgRW5qb3kgOkQ=:15:3:30:0:31:0:37:3:38:1:39:10:46:1:47:2:35:223469|1:64968478:2:Bloodbath but no:5:1:6:19747356:8:10:9:50:10:652130:12:0:13:21:14:51559:17::43:6:25::18:8:19:24992:42:0:45:23233:3:Qmxvb2RiYXRoLCBJdCdzIG5vdCBldmVuIHRoaXM=:15:3:30:0:31:0:37:0:38:1:39:8:46:1:47:2:35:706340|1:75795864:2:Bloodbath:5:2:6:12348083:8:10:9:40:10:88763:12:0:13:21:14:4326:17::43:5:25::18:7:19:25025:42:0:45:55947:3:VGhhbmtzIHRvIGV2ZXJ5b25lIGluIG15IGRpc2NvcmQgc2VydmVyIHRoYXQgY29udHJpYnV0ZWQ=:15:3:30:0:31:0:37:0:38:1:39:6:46:1:47:2:35:513064#503085:Riot:37415|3277407:Zyzyx:88354|12348083:KNOEPPEL:3009121|19747356:Texic:6152129#1~|~223469~|~2~|~ParagonX9 - HyperioxX~|~3~|~31~|~4~|~ParagonX9~|~5~|~3.77~|~6~|~~|~10~|~http%3A%2F%2Faudio.ngfiles.com%2F223000%2F223469_ParagonX9___HyperioxX.mp3~|~7~|~~|~8~|~1~:~1~|~467339~|~2~|~At the Speed of Light~|~3~|~52~|~4~|~Dimrain47~|~5~|~9.56~|~6~|~~|~10~|~http%3A%2F%2Fgeometrydashcontent.com%2Fsongs%2F467339.mp3~|~7~|~~|~8~|~1~:~1~|~513064~|~2~|~EnV - Uprise~|~3~|~149~|~4~|~Envy~|~5~|~8.71~|~6~|~~|~10~|~http%3A%2F%2Faudio.ngfiles.com%2F513000%2F513064_EnV---Uprise.mp3~|~7~|~UCaRqE7rKwJl1BvMRU4FFVJQ~|~8~|~1~:~1~|~706340~|~2~|~-At the Speed of Light- (8 bit Remix)~|~3~|~46724~|~4~|~ThaPredator~|~5~|~4.78~|~6~|~~|~10~|~http%3A%2F%2Faudio.ngfiles.com%2F706000%2F706340_-At-the-Speed-of-Light--8-.mp3~|~7~|~~|~8~|~1#4:0:10#1664b8bb919b0822a4408752c37a9fb5f651f813");
 
-TEST_CASE("StringDelim")
-{
-    auto sr = rrp::rrp<StringDelim>("1---2---3");
-    REQUIRE(sr.a == 1);
-    REQUIRE(sr.b == 2);
-    REQUIRE(sr.c == 3);
-}
+        std::string x = r.levels()[0].levelName();
 
-TEST_CASE("SimpleDefaultValues")
-{
-    auto sr = rrp::rrp<SimpleResponse>("a,b,c");
-    INFO(sr.a);
-    REQUIRE(sr.a == 5);
-    REQUIRE(sr.b == 6);
-    REQUIRE(sr.c == 7);
-}
+        "bloodbath"_test = [&](){
+            expect(r.levels()[0].levelID() == 10565740_i);
+            expect(r.levels()[0].levelName() == "Bloodbath");
+            expect(r.levels()[0].coins() == 0);
+            expect(r.levels()[0].epic() == 0);
+            expect(r.levels()[0].downloads() == 44138442_i);
+        };
 
 
-struct SimpleFloat
-{
-    float a, b, c;
-    constexpr static std::string_view DELIM = ",";
-};
-
-TEST_CASE("SimpleFloat")
-{
-    auto r = rrp::rrp<SimpleFloat>("1.0,2,3");
-    INFO(r.a << " " << r.b << " " << r.c);
-    REQUIRE(r.a == 1.0f);
-    REQUIRE(r.b == 2.0f);
-    REQUIRE(r.c == 3.0f);
-
-    r = rrp::rrp<SimpleFloat>("1.5356,2.79183,36.57132");
-    REQUIRE(r.a == 1.5356f);
-    REQUIRE(r.b == 2.79183f);
-    REQUIRE(r.c == 36.57132f);
-}
-
-struct Strings
-{
-    int a;
-    std::string b;
-    std::string c;
-    DELIM(",");
-};
-
-TEST_CASE("Strings")
-{
-    auto r = rrp::rrp<Strings>("1,hello world,test");
-    REQUIRE(r.a == 1);
-    REQUIRE(r.b == "hello world");
-    REQUIRE(r.c == "test");
-}
-
-struct NestedA
-{
-    int a, b, c;
-    DELIM(",")
-    struct NestedB
-    {
-        float a, b, c;
-        DELIM("-")
-    } nested;
-};
-
-TEST_CASE("Nested")
-{
-    auto r = rrp::rrp<NestedA>("1,2,3,1.5-2.3-7.7");
-    REQUIRE(r.a == 1);
-    REQUIRE(r.b == 2);
-    REQUIRE(r.c == 3);
-    REQUIRE(r.nested.a == 1.5f);
-    REQUIRE(r.nested.b == 2.3f);
-    REQUIRE(r.nested.c == 7.7f);
-}
-
-
-
-/*GETGJLEVELS*/
-
-struct LevelObject
-{
-    int id;
-    std::string levelName;
-    DELIM(",");
-};
-
-struct Creator
-{
-    int userID;
-    std::string userName;
-    int accountID;
-    DELIM(":");
-};
-
-struct GetGJLevels21
-{
-    LevelObject level;
-    Creator creator;
-    DELIM("#");
-};
-
-TEST_CASE("GetGJLevels21")
-{
-    auto r = rrp::rrp<GetGJLevels21>("1,Stereo Madness#1:robtop:1");
-    REQUIRE(r.level.id == 1);
-    REQUIRE(r.level.levelName == "Stereo Madness");
-    REQUIRE(r.creator.userID == 1);
-    REQUIRE(r.creator.userName == "robtop");
-    REQUIRE(r.creator.accountID == 1);
+        expect(r.levels()[1].levelName() == "Bloodbath Z");
+    
+    };
 }
